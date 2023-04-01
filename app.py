82409255d147f7023284561099c0d3f1a5523d39
Brunvelop <brunvelop@gmail.com>
2023-04-01 20:24:26 +0200

    Feat: Try optimize on banana

--- app.py
+++ app.py
@@ -1,34 +1,56 @@
+import torch
 import base64
 from io import BytesIO
-from sd import StableDifussion2
-
+from diffusers import DiffusionPipeline
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
-    model = StableDifussion2("nitrosocke/Future-Diffusion")
+    model = DiffusionPipeline.from_pretrained(
+        "nitrosocke/Future-Diffusion",
+        torch_dtype=torch.float16
+    ).to('cuda')
 
+
+def _generate_latent(height, width, seed=None, device="cuda"):
+    generator = torch.Generator(device=device)
+
+    # Get a new random seed, store it and use it as the generator state
+    if not seed:
+        seed = generator.seed()
+    generator = generator.manual_seed(seed)
+    
+    image_latent = torch.randn(
+        (1, model.unet.in_channels, height // 8, width // 8),
+        generator = generator,
+        device = device
+    )
+    return image_latent.type(torch.float16)
+    
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
     global model
 
-    # Parse out your arguments
-    prompt = "future style "+ model_inputs.get('prompt', None) +" cinematic lights, trending on artstation, avengers endgame, emotional"
-    negative_prompt="duplicate heads bad anatomy extra legs text"
-    height=64*6 
-    width=64*6
-    if prompt == None:
-        return {'message': "No prompt provided"}
+    latent = _generate_latent(64*6, 64*6)
+    images = model(
+        prompt = "future style "+ model_inputs.get('prompt', None) +" cinematic lights, trending on artstation, avengers endgame, emotional",
+        height=64*6,
+        width=64*6,
+        num_inference_steps = 20,
+        guidance_scale = 7.5,
+        negative_prompt="duplicate heads bad anatomy extra legs text",
+        num_images_per_prompt = 1,
+        return_dict=False,
+        latents = latent
+    )
+    image = images[0][0]
     
-    # Run the model
-    image = model.generate_image(prompt, negative_prompt, height, width)
-
     # Resize output and conver to base64
     image = image.resize((250, 250))
     buffered = BytesIO()
     image.save(buffered, format="JPEG")
-    image_64 = str(base64.b64encode(buffered.getvalue()))[2:-1]
+    image_base64 = str(base64.b64encode(buffered.getvalue()))[2:-1]
 
     # Return the results as a dictionary
-    return {"image_64":image_64}
+    return {"image_base64":image_base64}
